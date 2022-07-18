75b2e5123c6e68eca088c3cd9c4b6e24685a1053
guangzheng.li <iguangzhengli@gmail.com>
2022-07-18 20:44:13 +0800

    [container] refactor: use golang http server and optimize container image size to 20MB from 60MB

--- container/app.rb
+++
@@ -1,8 +0,0 @@
-# app.rb
-require "sinatra"
-
-set :bind, "0.0.0.0"
-
-get "*" do
-  "[v1] Hello, Kubernetes!\n"
-end
---
+++ container/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"io"
+	"net/http"
+)
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "[v1] Hello, Kubernetes!")
+}
+
+func main() {
+	http.HandleFunc("/", hello)
+	http.ListenAndServe(":8080", nil)
+}
