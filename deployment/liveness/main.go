c5e1b3b6a2bdd9a3c1b7368fecbd652d60171b3b
guangzheng.li <iguangzhengli@gmail.com>
2022-08-26 20:00:40 +0800

    [deployment] feat: add deployment liveness tutorial.

---
+++ deployment/liveness/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"time"
+)
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "[v2] Hello, Kubernetes!")
+}
+
+func main() {
+	started := time.Now()
+	http.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		duration := time.Since(started)
+		if duration.Seconds() > 15 {
+			w.WriteHeader(500)
+			w.Write([]byte(fmt.Sprintf("error: %v", duration.Seconds())))
+		} else {
+			w.WriteHeader(200)
+			w.Write([]byte("ok"))
+		}
+	})
+
+	http.HandleFunc("/", hello)
+	http.ListenAndServe(":3000", nil)
+}
