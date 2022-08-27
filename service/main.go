522391b0c88218566415903f4868dad21c8f605b
guangzheng.li <iguangzhengli@gmail.com>
2022-08-27 12:22:30 +0800

    [service] feat: add ClusterIp service type tutorial.

---
+++ service/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	host, _ := os.Hostname()
+	io.WriteString(w, fmt.Sprintf("[v3] Hello, Kubernetes!, From host: %s", host))
+}
+
+func main() {
+	http.HandleFunc("/", hello)
+	http.ListenAndServe(":3000", nil)
+}
